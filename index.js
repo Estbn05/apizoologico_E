8427166e66ffab7fa8ac87acf91a4d1568879d3d
Estbn05 <yefrye.avilaz@konradlorenz.edu.co>
2025-09-11 19:22:39 -0500

    adelanto de express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+// 'use restrict';
+// const http = require('http');
 
+// const server= http.createServer(function(req,res){
+//     res.writeHead(200, {'content-type': 'text/plain'});
+//     res.end('Hola mundo');
+// });
+
+// server.listen(5000);
+
+const express = require('express') //require es igual a import 
+const app = express()
+const port = 3000
+
+app.get('/',(req,res) =>{ //request y response
+    res.send('Hola Mundo') //usamos el request cuando necesitamos jalar cualquier tipo de informacion 
+})
+
+app.listen(port, () =>{
+    console.log('la app se esta ejecutando')
+})
