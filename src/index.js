7e7b6dd6950ac9981023340a705d9cff14c2392c
Estbn05 <yefrye.avilaz@konradlorenz.edu.co>
2025-09-11 20:35:17 -0500

    quedamos en la creacion de animalModel

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express()
 const port = 3000
 
 app.get('/',(req,res) =>{ //request y response
-    res.send('Hola Mundo spy estebitan') //usamos el request cuando necesitamos jalar cualquier tipo de informacion 
+    res.send('Hola Mundo soy estebitan') //usamos el request cuando necesitamos jalar cualquier tipo de informacion 
 })
 
 app.listen(port, () =>{
